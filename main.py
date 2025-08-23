8c32d72f7f8e3bf046b91b39b27496eae11ec884
dargem <genericz145@gmail.com>
2025-08-23 12:20:19 +1000

    better translation prompts, timeout works to avoid repetition

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ class TranslationState(TypedDict):
     language: str
     translation: str
     feedback: str
+    feedback_rout_loops: int
 
 
-llm = GoogleGenerativeAI(model="gemini-2.5-flash-lite", temperature=0)
+llm = GoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.7)
 
 
 def language_detector_node(state: TranslationState):
@@ -38,11 +39,22 @@ def language_detector_node(state: TranslationState):
 def translator_node(state: TranslationState):
     print("translating text...")
 
-    template = "Translate the following {language} text to English.\n\nText:{text}"
+    template = """
+    You are a professional translator specialising in fiction. 
+    You work with Chinese to English translations and are highly proficient in localisation.
+    Prioritise fluency while maintaining semantic meaning.
+    Translate the following {language} text to English.
+    Text: {text}
+    """
     if "translation" in state:
-        template += "\n\nYour prior translation was {translation}"
-        template += "\n\nYour feedback was {feedback}"
-        template += "\n\nYour updated translation:"
+        template += """
+        Your prior translation was: 
+        {translation}
+        Your feedback was: 
+        {feedback}
+        With this feedback incorporated, create a richer response.
+        Your updated translation, incorporating feedback:
+        """
         prompt = PromptTemplate(
             input_variables=["text", "language", "feedback", "translation"],
             template=template,
@@ -55,7 +67,7 @@ def translator_node(state: TranslationState):
                 translation=state["translation"],
             )
         )
-        print(state["feedback"])
+        #print(state["feedback"])
     else:
         template+= "\n\n Translation:"
         prompt = PromptTemplate(
@@ -79,9 +91,16 @@ def junior_editor_node(state: TranslationState):
         template="""
         Evaluate the quality of the following translation for the text.
         Be highly critical in your evaluation, you only want the very best.
-        If it is of high enough quality return the word \"approved response\" in your response else give feedback for improvement.
-        Text: {text}
-        Translation: {translation}
+        Be harsh but reasonable.
+        If it is of high enough quality return the words \"approved response accepted\", review by the following:
+        - readability
+        - fluency
+        - reading level
+        - consistency of terminology
+        - semantic accuracy
+        Produce a list of specific errors/suggestions with justifications and avoid a general conclusion.
+        Original Text: {text}
+        Translation for assessment: {translation}
         """,
     )
     message = HumanMessage(
@@ -90,24 +109,41 @@ def junior_editor_node(state: TranslationState):
     feedback = llm.invoke([message]).strip()
     return {"feedback": feedback}
 
+def increment_translate_feedback_node(state: TranslationState) -> TranslationState:
+    if "feedback_rout_loops" not in state:
+        state["feedback_rout_loops"] = 0
+    state["feedback_rout_loops"]+=1
+    return state
+
 
 # conditional logic
-def route_junior_check(state: TranslationState) -> bool:
-    return "approved response" in state["feedback"]
+def route_junior_pass(state: TranslationState) -> bool:
+    return "approved response accepted" in state["feedback"]
 
+
+def rout_increment_exceed(state: TranslationState) -> bool:
+    """If its past the max number of feedback loops, skip the junior editor"""
+    return state["feedback_rout_loops"]>=3
 
 # creating the graph
 workflow = StateGraph(TranslationState)
 workflow.add_node("language_detector_node", language_detector_node)
 workflow.add_node("translator_node", translator_node)
 workflow.add_node("junior_editor_node", junior_editor_node)
+workflow.add_node("increment_translate_feedback_node", increment_translate_feedback_node)
 
 workflow.set_entry_point("language_detector_node")
 workflow.add_edge("language_detector_node", "translator_node")
-workflow.add_edge("translator_node", "junior_editor_node")
+workflow.add_edge("translator_node", "increment_translate_feedback_node")
+workflow.add_conditional_edges(
+    "increment_translate_feedback_node", 
+    rout_increment_exceed,
+    path_map={True: END, False: "junior_editor_node"}
+)
+
 workflow.add_conditional_edges(
     "junior_editor_node",
-    route_junior_check,
+    route_junior_pass,
     path_map={True: END, False: "translator_node"},
 )
 app = workflow.compile()
@@ -122,5 +158,5 @@ if __name__ == "__main__":
     """
     state_input = {"text": sample_text}
     result = app.invoke(state_input)
-    for key in result:
+    for key in [string_key for string_key in result if isinstance(result[string_key], str)]:
         print(key + " " + result[key])
